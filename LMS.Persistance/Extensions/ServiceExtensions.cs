16415df4dbaf406b005a552bbee1bd500b54b08e
Mikael Fiala-Lundstrom <millom@gmail.com>
2024-09-17 10:29:37 +0200

    LMS.Persistance: Add ServiceExtensions, setup db

---
+++ LMS.Persistance/Extensions/ServiceExtensions.cs
@@ -0,0 +1,17 @@
+﻿using LMS.API;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LMS.Persistance.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureSql(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'MyDbContext' not found.")));
+        }
+    }
+}
