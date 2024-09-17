7796a8d02f09de0c91268008ac467d239ec0ea88
Mikael Fiala-Lundstrom <millom@gmail.com>
2024-09-17 11:30:22 +0200

    LMS.Persistance: Add DbSets for all entities

--- LMS.Persistance/ApplicationDbContext.cs
+++ LMS.Persistance/ApplicationDbContext.cs
@@ -7,6 +7,11 @@ namespace LMS.API
 		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
 		{
 		}
-		public DbSet<User> Users { get; set; }
-	}
+
+		public DbSet<User> Users => Set<User>();
+		public DbSet<Course> Courses => Set<Course>();
+		public DbSet<Module> Modules => Set<Module>();
+		public DbSet<Activity> Activitys => Set<Activity>();
+		public DbSet<ActivityType> ActivityTypes => Set<ActivityType>();
+    }
 }
