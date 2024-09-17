4b478344415938237e69df79d09f83c1f5268195
Mikael Fiala-Lundstrom <millom@gmail.com>
2024-09-17 12:03:23 +0200

    CoursesController: Some cleanup

--- LMS.Presentation/Controllers/CoursesController.cs
+++ LMS.Presentation/Controllers/CoursesController.cs
@@ -1,10 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Web.Http;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using System.Web.Http;
 
-using Microsoft.AspNetCore.Http;
+//using Microsoft.AspNetCore.Http;
+//using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
