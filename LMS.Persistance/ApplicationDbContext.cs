88ffbb6f465f275d4c1ca2075e43a6f1d6b894c5
Mikael Fiala-Lundstrom <millom@gmail.com>
2024-09-17 16:22:58 +0200

    ApplicationDbContext: DbContext -> IdentityDbContext

--- LMS.Persistance/ApplicationDbContext.cs
+++ LMS.Persistance/ApplicationDbContext.cs
@@ -1,17 +1,30 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using LMS.Models.Entities;
+
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace LMS.API
 {
-	public class ApplicationDbContext : DbContext
-	{
+    //public class MovieCardsAPIContext : IdentityDbContext<ApplicationUser, IdentityRole<long>, long>
+    //{
+    //public MovieCardsAPIContext(DbContextOptions<MovieCardsAPIContext> options)
+    //    : base(options)
+    //{
+
+
+    //public class ApplicationDbContext : IdentityDbContext<ApplicationUser, IdentityRole<string>, string>
+    public class ApplicationDbContext : IdentityDbContext<User, IdentityRole<string>, string>
+    {
 		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
 		{
 		}
 
-		public DbSet<User> Users => Set<User>();
+		//public DbSet<User> Users => Set<User>();
 		public DbSet<Course> Courses => Set<Course>();
 		public DbSet<Module> Modules => Set<Module>();
 		public DbSet<Activity> Activitys => Set<Activity>();
 		public DbSet<ActivityType> ActivityTypes => Set<ActivityType>();
+		//public DbSet<ApplicationUser> ApplicationUsers => Set<ApplicationUser>();
     }
 }
