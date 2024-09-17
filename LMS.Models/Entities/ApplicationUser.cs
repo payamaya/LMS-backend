ec97686ea492cf384ce780dcfd3c6bbb47dbb614
Mikael Fiala-Lundstrom <millom@gmail.com>
2024-09-17 16:17:59 +0200

    Models/Entities: Remove ApplicationUser, use User instead

--- LMS.Models/Entities/ApplicationUser.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace LMS.Models.Entities;
-
-public class ApplicationUser : IdentityUser
-{
-    public string? RefreshToken { get; set; }
-    public DateTime RefreshTokenExpireTime { get; set; }
-}
---
+++ LMS.Models/Entities/User_NotUsed.cs
@@ -0,0 +1,14 @@
+﻿using LMS.Models.Entities;
+
+namespace LMS.API
+{
+    public class User_NotUsed
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Email { get; set; }
+
+		public int CourseId { get; set; }
+		public Course Course { get; set; }
+	}
+}
