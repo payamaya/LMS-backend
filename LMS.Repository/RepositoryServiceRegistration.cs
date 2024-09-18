624b3943342c1f5477d9237a1afff3836368f790
payamaya <paulyashou81@gmail.com>
2024-09-18 16:01:38 +0200

    Repository : Add dependency injection
    Add extensions

---
+++ LMS.Repository/RepositoryServiceRegistration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using LMS.Repository.Extensions;
+
+namespace LMS.Repository
+{
+    public static class RepositoryServiceRegistration
+    {
+        public static IServiceCollection AddPresentationServices(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.ConfigureRepositories();
+
+            return services;
+        }
+    }
+}
