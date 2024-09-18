cbdc9bd61adf94f20c2dc7ecedc62cb8d64e3b83
Mikael Fiala-Lundstrom <millom@gmail.com>
2024-09-18 10:52:31 +0200

    Persistance: Some cleanup

--- LMS.Persistance/Extensions/ServiceExtensions.cs
+++ LMS.Persistance/Extensions/ServiceExtensions.cs
@@ -1,6 +1,4 @@
-﻿using LMS.API;
-
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
