69f40f60c966d0bc14d66c412621001732fa1169
payamaya <paulyashou81@gmail.com>
2024-09-19 14:58:52 +0200

    Change int to Guid

--- LMS.Models/Entities/User_NotUsed.cs
+++ LMS.Models/Entities/User_NotUsed.cs
@@ -4,11 +4,11 @@ namespace LMS.API
 {
     public class User_NotUsed
 	{
-		public int Id { get; set; }
+		public Guid Id { get; set; }
 		public string Name { get; set; }
 		public string Email { get; set; }
 
-		public int CourseId { get; set; }
+		public Guid CourseId { get; set; }
 		public Course Course { get; set; }
 	}
 }
