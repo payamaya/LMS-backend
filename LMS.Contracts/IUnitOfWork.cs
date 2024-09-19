940d178456cc4c1b1971393397973e2873687ad4
payamaya <paulyashou81@gmail.com>
2024-09-19 11:43:33 +0200

    - UnitOfWork : Add ICourseRepository
    - IUnitOfwork : Add ICourseRepository

--- LMS.Contracts/IUnitOfWork.cs
+++ LMS.Contracts/IUnitOfWork.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LMS.Contracts
+﻿namespace LMS.Contracts
 {
     public interface IUnitOfWork
     {
      /*   IMovieRepository Movie { get; }
-
         IActorRepository Actor { get; }*/
 
+        ICourseRepository Course { get; }
         Task CompleteAsync();
     }
 }
--- LMS.Repository/UnitOfWork.cs
+++ LMS.Repository/UnitOfWork.cs
@@ -1,10 +1,5 @@
 ﻿using LMS.Contracts;
 using LMS.Persistance;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LMS.Repository
 {
@@ -15,7 +10,8 @@ namespace LMS.Repository
         //private readonly Lazy<IActorRepository> _actorRepository;
 
         public ICourseRepository Course => _courseRepository.Value;
-       // public IActorRepository Actor => _actorRepository.Value;
+        // public IActorRepository Actor => _actorRepository.Value;
+
 
         public UnitOfWork(ApplicationDbContext context, Lazy<ICourseRepository> courseRepository)
         {
