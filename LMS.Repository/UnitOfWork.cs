ceceff711528304db7c899ff3ec39d1f88c4c181
Mikael Fiala-Lundstrom <millom@gmail.com>
2024-09-20 12:15:17 +0200

    Repository/UnitOfWork: Add all lazy parameters for usage

--- LMS.Repository/UnitOfWork.cs
+++ LMS.Repository/UnitOfWork.cs
@@ -15,6 +15,10 @@ namespace LMS.Repository
         //private readonly Lazy<IActorRepository> _actorRepository;
 
         public ICourseRepository Course => _courseRepository.Value;
+        public IModuleRepository Module => _moduleRepository.Value;
+        public IActivityRepository Activity => _activityRepository.Value;
+        public IActivityTypeRepository ActivityType => _activityTypeRepository.Value;
+        public IUserRepository User => _userRepository.Value;
         // public IActorRepository Actor => _actorRepository.Value;
 
 
@@ -26,18 +30,23 @@ namespace LMS.Repository
             Lazy<IModuleRepository> moduleRepository,
             Lazy<IUserRepository> userRepository)
         {
-            _context = context ?? throw new ArgumentNullException(nameof(context));
+            _context = context
+                ?? throw new ArgumentNullException(nameof(context));
+
             _courseRepository = courseRepository
                 ?? throw new ArgumentNullException(nameof(courseRepository));
+
             _activityTypeRepository = activityTypeRepository
                 ?? throw new ArgumentNullException(nameof(activityTypeRepository));
+
             _activityRepository = activityRepository
                 ?? throw new ArgumentNullException(nameof(activityRepository));
+
             _moduleRepository = moduleRepository
                 ?? throw new ArgumentNullException(nameof(moduleRepository));
+
             _userRepository = userRepository
                 ?? throw new ArgumentNullException(nameof(userRepository));
-            // _actorRepository = actorRepository;
         }
 
         public async Task CompleteAsync()
