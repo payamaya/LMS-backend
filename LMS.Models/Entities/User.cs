1bc1378ec59a4d50f26cf92b8659d03910ef00d2
Mikael Fiala-Lundstrom <millom@gmail.com>
2024-09-24 09:38:33 +0200

    Entities/User: Add IsStudnet not mapped prop

--- LMS.Models/Entities/User.cs
+++ LMS.Models/Entities/User.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Identity;
 
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.Tracing;
+
 namespace LMS.Models.Entities;
 
 //public class ApplicationUser : IdentityUser
@@ -8,6 +11,8 @@ public class User : IdentityUser
     //public int Id { get; set; }
     public string Name { get; set; }
     //public string Email { get; set; }
+    [NotMapped]
+    public bool IsStudent { get; set; }
 
     public Guid? CourseId { get; set; }
     public Course? Course { get; set; }
