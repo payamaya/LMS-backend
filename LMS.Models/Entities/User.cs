1547c916129f9db8ee35686918105166464fb7a2
Mikael Fiala-Lundstrom <millom@gmail.com>
2024-09-25 10:30:11 +0200

    Entities/User: Remove NotMapped for IsStudent, add to ef

--- LMS.Models/Entities/User.cs
+++ LMS.Models/Entities/User.cs
@@ -11,7 +11,7 @@ public class User : IdentityUser
     //public int Id { get; set; }
     public string Name { get; set; }
     //public string Email { get; set; }
-    [NotMapped]
+
     public bool IsStudent { get; set; }
 
     public Guid? CourseId { get; set; }
