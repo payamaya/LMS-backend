206ddb440fe80aec46a530e0c1e59267a623fc72
vikkoooo <vikkolundberg@gmail.com>
2024-10-01 09:07:25 +0200

    fix merge errors

--- LMS.Presentation/Controllers/CoursesController.cs
+++ LMS.Presentation/Controllers/CoursesController.cs
@@ -12,146 +12,132 @@ using LMS.Models.Entities;
 using LMS.Persistance;
 using LMS.Service.Contracts;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-
-using System.Runtime.CompilerServices;
-using System.Security.Claims;
 
 namespace LMS.Presentation.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class CoursesController : ControllerBase
-    {
-        private readonly ApplicationDbContext _context;
-        private readonly IServiceManager _sm;
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CoursesController : ControllerBase
+	{
+		private readonly ApplicationDbContext _context;
+		private readonly IServiceManager _sm;
 
-        public CoursesController(ApplicationDbContext context, IServiceManager sm)
-        {
-            _context = context;
-            _sm = sm;
-        }
+		public CoursesController(ApplicationDbContext context, IServiceManager sm)
+		{
+			_context = context;
+			_sm = sm;
+		}
 
-        // GET: api/Courses
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <returns></returns>
-        [HttpGet]
-        //[Authorize]
-        //[Authorize(Roles = "Teacher")]
-        //[Authorize(Roles = "Student")]
-        //[Authorize(Roles = "Teacher")]
-        [Produces("application/json")]
-        public async Task<ActionResult<IEnumerable<CourseDto>>> GetCourses()
-        {
-           return Ok(await _sm.CourseService.GetCoursesAsync());
-            /*return await _context.Courses.ToListAsync();*/
-        }
+		// GET: api/Courses
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <returns></returns>
+		[HttpGet]
+		//[Authorize]
+		//[Authorize(Roles = "Teacher")]
+		//[Authorize(Roles = "Student")]
+		//[Authorize(Roles = "Teacher")]
+		[Produces("application/json")]
+		public async Task<ActionResult<IEnumerable<CourseDto>>> GetCourses()
+		{
+			return Ok(await _sm.CourseService.GetCoursesAsync());
+			/*return await _context.Courses.ToListAsync();*/
+		}
 
-        // GET: api/Courses/5
-        /// <summary>
-        /// Hur gör man det här, 3 * /
-        /// </summary>
-        /// <returns></returns>
-        [HttpGet("Student")]
-        //[Authorize]
-        //[Authorize(Roles = "Teacher")]
-        //[OverrideAuthorization]
-        //[Authorize(Roles = "Student")]
-        [Produces("application/json")]
-        public async Task<ActionResult<CourseDetailedDto>> GetCourse()
-        {
-            var course = await _sm.CourseService.GetCourseAsync(User);
+		// GET: api/Courses/5
+		/// <summary>
+		/// Hur gör man det här, 3 * /
+		/// </summary>
+		/// <returns></returns>
+		[HttpGet("Student")]
+		//[Authorize]
+		//[Authorize(Roles = "Teacher")]
+		//[OverrideAuthorization]
+		//[Authorize(Roles = "Student")]
+		[Produces("application/json")]
+		public async Task<ActionResult<CourseDetailedDto>> GetCourse()
+		{
+			var course = await _sm.CourseService.GetCourseAsync(User);
 
-            return Ok(course);
-        }
+			return Ok(course);
+		}
 
-        // GET: api/Courses/5
-        /// <summary>
-        /// Hur gör man det här, 3 * /
-        /// </summary>
-        /// <param name="id">Hej hej</param>
-        /// <returns></returns>
-        [HttpGet("{id:guid}")]
-        //[Authorize]
-        //[Authorize(Roles = "Teacher")]
-        //[Authorize(Roles = "Student")]
-        //[Authorize(Roles = "Teacher")]
-        [Produces("application/json")]
-        public async Task<ActionResult<CourseDetailedDto>> GetCourse(Guid id)
-        {
-            var course = await _sm.CourseService.GetCourseAsync(id);
+		// GET: api/Courses/5
+		/// <summary>
+		/// Hur gör man det här, 3 * /
+		/// </summary>
+		/// <param name="id">Hej hej</param>
+		/// <returns></returns>
+		[HttpGet("{id:guid}")]
+		//[Authorize]
+		//[Authorize(Roles = "Teacher")]
+		//[Authorize(Roles = "Student")]
+		//[Authorize(Roles = "Teacher")]
+		[Produces("application/json")]
+		public async Task<ActionResult<CourseDetailedDto>> GetCourse(Guid id)
+		{
+			var course = await _sm.CourseService.GetCourseAsync(id);
 
-            return Ok(course);
-        }
+			return Ok(course);
+		}
 
-        // PUT: api/Courses/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-   /*     [HttpPut("{id}")]
-        public async Task<IActionResult> PutCourse(int id, Course course)
-        {
-            if (id != course.Id)
-            {
-                return BadRequest();
-            }
+		// PUT: api/Courses/5
+		// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+		/*     [HttpPut("{id}")]
+			 public async Task<IActionResult> PutCourse(int id, Course course)
+			 {
+				 if (id != course.Id)
+				 {
+					 return BadRequest();
+				 }
 
-            _context.Entry(course).State = EntityState.Modified;
+				 _context.Entry(course).State = EntityState.Modified;
 
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!CourseExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+				 try
+				 {
+					 await _context.SaveChangesAsync();
+				 }
+				 catch (DbUpdateConcurrencyException)
+				 {
+					 if (!CourseExists(id))
+					 {
+						 return NotFound();
+					 }
+					 else
+					 {
+						 throw;
+					 }
+				 }
 
-            return NoContent();
-        }*/
+				 return NoContent();
+			 }*/
 
-        // POST: api/Courses
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        //[Authorize(Roles = "Teacher")]
-        public async Task<ActionResult<Course>> PostCourse(Course course)
-        {
-            _context.Courses.Add(course);
-            await _context.SaveChangesAsync();
+		// POST: api/Courses
+		// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+		[HttpPost]
+		//[Authorize(Roles = "Teacher")]
+		public async Task<ActionResult<Course>> PostCourse(Course course)
+		{
+			_context.Courses.Add(course);
+			await _context.SaveChangesAsync();
 
-            return CreatedAtAction("GetCourse", new { id = course.Id }, course);
-        }
+			return CreatedAtAction("GetCourse", new { id = course.Id }, course);
+		}
 
-        // DELETE: api/Courses/5
-        [HttpDelete("{id}")]
-<<<<<<< Updated upstream
-//<<<<<<< HEAD
-        [Authorize(Roles = "Teacher")]
-        public async Task<IActionResult> DeleteCourse(Guid id)
-//=======
-//        public async Task<IActionResult> DeleteCourseAsync(Guid id)
-//>>>>>>> 6284b88 (Delete Method for All controller except user and activityType not finished yet)
-=======
-        //[Authorize(Roles = "Teacher")]
-        public async Task<IActionResult> DeleteCourseAsync(Guid id)
->>>>>>> Stashed changes
-        {
-            await _sm.CourseService.DeleteCourseAsync(id);
-            return NoContent();
-        }
+		// DELETE: api/Courses/5
+		[HttpDelete("{id}")]
+		[Authorize(Roles = "Teacher")]
+		public async Task<IActionResult> DeleteCourseAsync(Guid id)
+		{
+			await _sm.CourseService.DeleteCourseAsync(id);
+			return NoContent();
+		}
 
-        /*private bool CourseExists(int id)
+		/*private bool CourseExists(int id)
         {
             return _context.Courses.Any(e => e.Id == id);
         }*/
-    }
+	}
 }
--- LMS.Service/CourseService.cs
+++ LMS.Service/CourseService.cs
@@ -12,94 +12,80 @@ using System.Security.Claims;
 
 namespace LMS.Service
 {
-    public class CourseService : ICourseService
+	public class CourseService : ICourseService
 
-    {
-        private readonly UserManager<User> _userManager;
-        private readonly IUnitOfWork _uow;
-        private readonly IMapper _mapper;
+	{
+		private readonly UserManager<User> _userManager;
+		private readonly IUnitOfWork _uow;
+		private readonly IMapper _mapper;
 
-        public CourseService(
-            UserManager<User> userManager,
-            IUnitOfWork uow,
-            IMapper mapper)
-        {
-            _userManager = userManager;
-            _uow = uow;
-            _mapper = mapper;
-        }
-<<<<<<< Updated upstream
+		public CourseService(
+			UserManager<User> userManager,
+			IUnitOfWork uow,
+			IMapper mapper)
+		{
+			_userManager = userManager;
+			_uow = uow;
+			_mapper = mapper;
+		}
+		public async Task<CourseDetailedDto?> GetCourseAsync(Guid courseId, bool trackChanges = false)
+		{
+			var course = await _uow.Course.GetCourseAsync(courseId, trackChanges);
+			if (course is null)
+			{
+				throw new NotFoundException(nameof(GetCourseAsync), courseId);
+				//return null; //ToDo: Fix later
+			}
 
-//<<<<<<< HEAD
-        public async Task<CourseDetailedDto?> GetCourseAsync(Guid courseId, bool trackChanges = false)
-//=======
-     
+			return _mapper.Map<CourseDetailedDto>(course);
+		}
 
-//        public async Task<CourseDto?> GetCourseAsync(Guid courseId, bool trackChanges = false)
-//>>>>>>> 6284b88 (Delete Method for All controller except user and activityType not finished yet)
-=======
-     
+		// Bad request should never reach here, sent to Unauthorized middleware
+		public async Task<CourseDetailedDto?> GetCourseAsync(ClaimsPrincipal? userClaim, bool trackChanges = false)
+		{
+			if (userClaim == null) throw new BadRequestException($"Bad user claims");
 
-        public async Task<CourseDetailedDto?> GetCourseAsync(Guid courseId, bool trackChanges = false)
+			var user = await _userManager.GetUserAsync(userClaim);
+			if (user == null) throw new BadRequestException($"User not found");
 
->>>>>>> Stashed changes
-        {
-            var course = await _uow.Course.GetCourseAsync(courseId, trackChanges);
-            if (course is null)
-            {
-                throw new NotFoundException(nameof(GetCourseAsync), courseId);
-                //return null; //ToDo: Fix later
-            }
+			var roles = await _userManager.GetRolesAsync(user);
+			if (roles == null || !roles.Contains("Student"))
+				throw new BadRequestException($"Role not found");
 
-            return _mapper.Map<CourseDetailedDto>(course);
-        }
+			var courseId = (Guid)user.CourseId!;
 
-        // Bad request should never reach here, sent to Unauthorized middleware
-        public async Task<CourseDetailedDto?> GetCourseAsync(ClaimsPrincipal? userClaim, bool trackChanges = false)
-        {
-            if (userClaim == null) throw new BadRequestException($"Bad user claims");
+			var course = await _uow.Course.GetCourseAsync(courseId, trackChanges);
+			if (course is null)
+			{
+				throw new NotFoundException(nameof(GetCourseAsync), courseId);
+				//return null; //ToDo: Fix later
+			}
 
-            var user = await _userManager.GetUserAsync(userClaim);
-            if (user == null) throw new BadRequestException($"User not found");
+			return _mapper.Map<CourseDetailedDto>(course);
+		}
 
-            var roles = await _userManager.GetRolesAsync(user);
-            if (roles == null || !roles.Contains("Student"))
-                throw new BadRequestException($"Role not found");
-            
-            var courseId = (Guid)user.CourseId!;
-            
-            var course = await _uow.Course.GetCourseAsync(courseId, trackChanges);
-            if (course is null)
-            {
-                throw new NotFoundException(nameof(GetCourseAsync), courseId);
-                //return null; //ToDo: Fix later
-            }
+		public async Task<IEnumerable<CourseDto>> GetCoursesAsync(bool trackChanges = false)
+		{
+			var courses = await _uow.Course.GetCoursesAsync(trackChanges);
+			if (courses is null) return null!; //ToDo: Fix later
 
-            return _mapper.Map<CourseDetailedDto>(course);
-        }
+			return _mapper.Map<IEnumerable<CourseDto>>(courses);
 
-        public async Task<IEnumerable<CourseDto>> GetCoursesAsync(bool trackChanges = false)
-        {
-            var courses = await _uow.Course.GetCoursesAsync(trackChanges);
-            if (courses is null) return null!; //ToDo: Fix later
+		}
 
-            return _mapper.Map<IEnumerable<CourseDto>>(courses);
 
-        }
-   
+		public async Task DeleteCourseAsync(Guid id)
+		{
 
-        public async Task DeleteCourseAsync(Guid id)
-        {
+			var course = await GetCourseBy(id)
+				?? throw new NotFoundException("Course", id);
+			_uow.Course.Delete(course);
+			await _uow.CompleteAsync();
 
-            var course = await GetCourseBy(id)
-                ?? throw new NotFoundException("Course", id);
-            _uow.Course.Delete(course);
-            await _uow.CompleteAsync();
-            
-        }
-        private async Task<Course?> GetCourseBy(Guid id) => 
-            await _uow.Course.GetCourseAsync(id, trackChanges: false);
+		}
+		private async Task<Course?> GetCourseBy(Guid id) =>
+			await _uow.Course.GetCourseAsync(id, trackChanges: false);
 
-      
-    }
+
+	}
 }
