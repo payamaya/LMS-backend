74abe990e6143e53d8638c1a338ab8b407738be4
OskarMartens <martens.oskar@gmail.com>
2024-10-02 10:28:59 +0200

    Making teacher optional in CourseDto

--- LMS.Infrastructure/Dtos/CourseDTO.cs
+++ LMS.Infrastructure/Dtos/CourseDTO.cs
@@ -6,7 +6,6 @@
         public string CourseName { get; init; }
         public string Description { get; init; }
         public DateTime StartDate { get; init; }
-
-        public UserDto Teacher { get; init; }
+        public UserDto? Teacher { get; init; }
     }
 }
