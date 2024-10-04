6366f4a3ffa568d5a7a4ea31b7dbb4efd47e370e
OskarMartens <martens.oskar@gmail.com>
2024-10-04 11:05:53 +0200

    Create a dto for posting teacher id when creating.

---
+++ LMS.Infrastructure/Dtos/CoursePostDto.cs
@@ -0,0 +1,10 @@
+﻿namespace LMS.Infrastructure.Dtos
+{
+    public class CoursePostDto
+    {
+        public string CourseName { get; init; }
+        public string Description { get; init; }
+        public DateTime StartDate { get; init; }
+        public Guid? TeacherId { get; init; }
+    }
+}
